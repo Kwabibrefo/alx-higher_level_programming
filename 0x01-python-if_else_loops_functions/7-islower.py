7fa3f36233e5e667ce1095f2a79f5992b23c9722
kwabibrefo <kbnbrefonimo@gmail.com>
2023-08-19 10:35:14 -0700

    determine lower cases

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def is lower(alph):
-    if (ord(alph) >= 97) and (ord(alpj) <= 122):
-        return true
+def islower(alph):
+    if (ord(alph) >= 97) and (ord(alph) <= 122):
+        return True
     else:
         return False
