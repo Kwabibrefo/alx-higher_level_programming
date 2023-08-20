f84deb9ce3bcb5b9bfdb76581ab32bdb8110f7a8
kwabibrefo <kbnbrefonimo@gmail.com>
2023-08-20 14:00:18 -0700

    print uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(alph):
-    for i in ord(alph):
-        if (i >=97) and (i <= 122):
+    for i in alph:
+        i = ord(i)
+        if (i >= 97) and (i <= 122):
             i -= 32
-            print("{:c}".format(i), end="")
-        else:
-            print(i)
+        print("{:c}".format(i), end="")
+    print("")
