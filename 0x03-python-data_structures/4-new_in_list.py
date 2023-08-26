0d365f34c42b7e360b531616c0cc863eabce99c3
kwabibrefo <kbnbrefonimo@gmail.com>
2023-08-26 13:39:32 -0700

    create edited copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     newlist = my_list.copy()
-    newlist[idx] = element
     if (idx >= 0) and (idx < len(my_list)):
+        newlist[idx] = element
         return (newlist)
     else:
         return (my_list)
