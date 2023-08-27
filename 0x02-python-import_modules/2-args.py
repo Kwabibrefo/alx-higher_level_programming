ff0392114271ba9469ff47d42574437b23b9d4c2
kwabibrefo <kbnbrefonimo@gmail.com>
2023-08-27 15:12:17 -0700

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-argc = len(argv)
-if argc < 2:
-    print("{} arguments.".format(argc - 1))
-else:
-    if argc == 2:
-        print("{} argument:".format(argc - 1))
-    else:
+    argc = len(argv)
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    elif argc > 2:
         print("{} arguments:".format(argc - 1))
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
     for n in range(1, argc):
         print("{}: {}".format(n, argv[n]))
