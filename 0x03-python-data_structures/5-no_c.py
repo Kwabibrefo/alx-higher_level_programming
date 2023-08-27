9266b5da0381e3c4eac48c92f4f3a59bae6411e6
kwabibrefo <kbnbrefonimo@gmail.com>
2023-08-27 16:08:23 -0700

    push my best

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    d = len(my_string)
-    new_string = ""
-    for i in range(d):
-        if my_string[i] != 'C' and my_string[i] != 'c':
-            new_string += my_string[i]
-    return new_string
+    new_string = []
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string.append(i)
+    return ''.join(new_string)
